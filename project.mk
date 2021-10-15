ea9861ed191945effe243ef7299bac6c1c1a9eda
Pranav <pranav.shankar02@gmail.com>
2021-10-15 14:34:09 +0530

    make file added

--- project.mk
+++ project.mk
@@ -1,1 +1,14 @@
-//makefile
+a.out: main.o encrypt.o decrypt.o  explain.o 
+    gcc main.o encrypt.o decrypt.o explain.o 
+
+main.o : main.c header.h 
+    gcc -c main.c 
+
+encrypt.o:encrypt.c header.h
+    gcc -c encrypt.c
+
+decrypt.o: decrypt.c header.h
+    gcc -c decrypt.c
+	
+explain.o: explain.c header.h
+    gcc -c explain.c
