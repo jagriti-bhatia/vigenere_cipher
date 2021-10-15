41fcf96ea8b4d56df58ee97af921d5b8bd464a74
OBLIVIKRAM <90594692+OBLIVIKRAM@users.noreply.github.com>
2021-10-15 23:53:17 +0530

    issue project.mk
    created an makefile

--- project.mk
+++ project.mk
@@ -1,1 +1,5 @@
 //makefile
+$make -f Attester
+gcc -c main.c file_encrypt.c file_decrypt.c encrypt.c decrypt.c explain.c header.h
+gcc -o cipher main.o file_encrypt.o file_decrypt.o encrypt.o decrypt.o explain.o
+$
