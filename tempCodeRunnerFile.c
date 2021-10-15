3ca63e5cea390f3e0826f3728f828a8c2c0f0dc0
RheshaVinod <global-rhesha@gmail.com>
2021-10-15 22:55:23 +0530

    Revert "Added encryption for sentences"
    This reverts commit 4119c923fe4754f5882770bdd5c444b4634c0271.

--- tempCodeRunnerFile.c
+++
@@ -1,7 +0,0 @@
-printf("Enter the plaintext (text to be encrypted) \n");
-      scanf("%s", plaintext);
-      printf("Enter the key \n");
-      scanf("%s", key);
-      strupr(plaintext);
-      strupr(key);
-      encryption(plaintext, key);
