e78bb376474df0cc48615a6634e878340ee5152e
RheshaVinod <global-rhesha@gmail.com>
2021-10-15 23:01:23 +0530

    Added sentence encryption

--- encrypt.c
+++ encrypt.c
@@ -5,25 +5,36 @@
 void encryption(char text[],char key[])
 {
     int textlen = strlen(text), keylen = strlen(key), i, j;
+    int c=textlen;
     //defining array for encrypted message and key generated
     char newkey[textlen], encryptedmsg[textlen];
 
     //generating new key
-    for(i = 0, j = 0; i < textlen; ++i, ++j)
+    for(i = 0, j = 0; i < c; ++i, ++j)
 	  {
           //to repeat characters from start when the length of the ciphertext is > that of the keyword
         if(j == keylen)
             j = 0;
+        if(text[i]==' ')
+        c++;
 
         //to generate a new key from the given keyword such that the length of newkey = length of ciphertext
         newkey[i] = key[j];
     }
     newkey[i] = '\0'; //denote the end of a string
-
+    int k=0;
      
      for(i = 0; i < textlen; ++i)
+     {
+         if(text[i]== ' ')
+	 {
+		 encryptedmsg[i]=' ';
+		 continue;
+	 }
      //to convert the plain text to cipher text character wise using the vigenere cipher formula
-         encryptedmsg[i] = ((text[i] + newkey[i]) % 26) + 'A';
+         encryptedmsg[i] = ((text[i] + newkey[k]) % 26) + 'A';
+         k++;
+     }
      encryptedmsg[i] = '\0'; //denote the end of the string
 
 	 printf("\nNew Generated Key: %s", newkey);
